173483775c93874d30ee1671c3e6bf18881828a6
Quentin BRUNELLE <37067405+QuentinBrunelle@users.noreply.github.com>
2018-11-07 08:54:50 +0100

    Add files via upload

---
+++ PROJET_JAVASCRIPT/pop_mystere.js
@@ -0,0 +1,19 @@
+const pop = [
+    {
+		"nom": "Funko POP! Mystere"
+		"prix": "34.99";
+		"image": "Mystere";
+    },
+    
+    {
+		"nom": "Funko POP! Chevre"
+		"prix": "34.99";
+		"image": "Chevre"
+    },
+    
+    {
+		"nom": "Funko POP! Steak"
+		"prix": "34.99";
+		"image": "Steak"
+    }
+];
